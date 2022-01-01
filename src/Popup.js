ea2360c365f20ad998e89b834dd579f7afe0afe9
ethan <ksr20612@daum.net>
2022-01-01 15:52:44 +0900

    1. 수정일 : 2022.01.01 2. 수정 내용  - 팝업 추가

--- src/Popup.js
+++ src/Popup.js
@@ -1,7 +1,88 @@
-import React, {useState} from "react";
+import React, {useState,useLayoutEffect} from "react";
+import {createPortal} from "react-dom";
 
-const Popup = ({type}) => {
+const Popup = ({...props}) => {
     
+    const handleStart = () => {
+        props.handleClose();
+        if(props.callback){
+            props.callback();
+        }
+    }
+    const replay = () => {
+        props.handleClose();
+        if(props.init) {
+            props.init();
+        }
+    }
+
+    let $popup;
+    switch(props.type){
+    case "start":
+        $popup = (
+            <div className="popup start">
+                <div className="content">
+                    <div>지금부터 <bold>Stroop Test</bold>를 시작하겠습니다.</div>
+                    <div>화면에 나오는 낱말의 <bold>색</bold>을 가능한 빨리 키보드로 입력해주시면 됩니다.</div>
+                    <div><bold>{props.warmUpLen}회의 연습</bold> 후, 본 테스트를 진행하도록 하겠습니다.</div>
+                    <div>
+                        여러분의 타이핑 속도에 가중치를 두어 총 점수가 계산됩니다. <br></br>
+                        따라서 타자 속도에 대해서는 걱정하지 않으셔도 됩니다.
+                    </div>
+                    <div className="btn_start" onClick={handleStart}><span>시작하기</span></div>
+                </div>
+            </div>
+        );
+        break;
+    case "warmed_up":
+        console.log(props.score);
+        console.log(props.averageDuration);
+        $popup = (
+            <div className="popup warmed_up">
+                <div className="content">
+                    <div>연습이 종료되었습니다.</div>
+                    <div>
+                        평균 소요시간은 <bold>{props.averageDuration}초</bold>입니다. <br></br>
+                        정답률은 <bold>{props.score}%</bold>입니다.
+                    </div>
+                    <div>지금처럼 색을 입력해주신 후, <bold>엔터버튼</bold>을 눌러주시면 됩니다.</div>
+                    <div className="btn_list">
+                        <div onClick={replay}><span>다시 연습하기</span></div>
+                        <div onClick={handleStart}><span>시작하기</span></div>
+                    </div>
+                </div>
+            </div>
+        );
+        break;
+    case "end":
+        $popup = (
+            <div className="popup start">
+                <div className="content">
+                </div>
+            </div>
+        );
+        break;
+    case "error":
+        $popup = (
+            <div className="popup start">
+                <div className="content">
+                </div>
+            </div>
+        );
+        break;
+    default:
+        $popup = (
+            <div className="popup start">
+                <div className="content">
+                </div>
+            </div>
+        );
+        break;
+    }
+
+    return createPortal(
+        $popup, document.querySelector("#modal")
+    )
 
 }
 
--- src/TestSheet.js
+++ src/TestSheet.js
@@ -4,13 +4,14 @@ import WordScreen from "./WordScreen.js";
 import InputBox from "./InputBox.js";
 import Progress from "./Progress.js";
 import stopwatch from "./Stopwatch.js";
+import Popup from "./Popup.js";
 
 const TestSheet = () => {
 
     const [wordList, setWordList] = useState(WordList.warm_up);
     const [round, setRound] = useState(1);
     const [word, setWord] = useState("");
-    const [isPractice, setPractice] = useState(true);
+    const [step, setStep] = useState(0);
     const [isPopupOn, setPopupOn] = useState(false);
     const [answer, setAnswer] = useState("");
     const [correctness, setCorrectness] = useState([]); // 정답 : 1, 오답 : 0
@@ -18,9 +19,12 @@ const TestSheet = () => {
 
     const btnSubmit = useRef();
 
-    // 1 : practice, 2 : real
+    const [averageDuration,setAverageDuration] = useState(0);
+    const [score,setScore] = useState(0);
+
+    // 0 : start, 1 : practice, 2 : real
     const setType = (type) => {
-        setPractice(type===1? true : false);
+        setStep(type);
         setWordList(type===1? WordList.warm_up : WordList.real);
     }
     const changeRound = async () => {
@@ -29,7 +33,7 @@ const TestSheet = () => {
         await storeCurrentRound(stopwatch.end());
         if(round>=wordList.length) {
             console.log("종료");
-            closeRound(isPractice);
+            closeRound(step===1? true : false);
         }else {
             nextRound(round);
         }
@@ -50,6 +54,8 @@ const TestSheet = () => {
     const init = () => {
         setRound(1);
         setType(1);
+        setCorrectness([]);
+        setDuration([]);
         setWordList(WordList.warm_up);
         setWord(wordList[round-1]);
         stopwatch.start();
@@ -58,15 +64,18 @@ const TestSheet = () => {
         const reset = 1;
         setRound(reset);
         setType(2);
+        setCorrectness([]);
+        setDuration([]);
         setWordList(WordList.real);
         setWord(wordList[reset-1]);
         setAnswer("");
         stopwatch.start();
     }
-    const closeRound = (isPractice) => {
+    const closeRound = async (isPractice) => {
         console.log(isPractice);
         if(isPractice) {
-            return startReal();
+            init();
+            openPopup();
         }else {
             console.log("완전 종료");
         }
@@ -78,9 +87,36 @@ const TestSheet = () => {
         }
     }
 
+    const openPopup = () => {
+        if(!isPopupOn) {
+            setPopupOn(true);
+        }
+    }
+    const closePopup = () => {
+        if(isPopupOn) {
+            setPopupOn(false);
+        }
+    }
+
+    const calcScore = () => {
+
+        const total = wordList.length;
+        const currentScore = correctness.reduce((a,b)=>a*1+b*1);
+
+        setScore((currentScore/total).toFixed(2)*100);
+        setAverageDuration((duration.reduce((a,b)=>a*1+b*1)/total).toFixed(2));
+    }
+
     useLayoutEffect(()=>{
-        init();
+        openPopup();
     },[])
+
+    useLayoutEffect(()=>{
+        if(round>=wordList.length) {
+            calcScore();
+
+        }
+    })
 
     return (
         <>
@@ -89,6 +125,7 @@ const TestSheet = () => {
                 <WordScreen word={word}/>
                 <InputBox handleChange={handleChange} submit={changeRound} pushEnter={pushEnter} submitButton={btnSubmit}/>
             </div>
+            <div>{isPopupOn && <Popup handleClose={closePopup} type={step===0? "start" : step===1? "warmed_up" : "end"} warmUpLen={wordList.length} callback={step===0? init : step===1? startReal : ""} init={init} score={score} averageDuration={averageDuration}></Popup>}</div>
         </>
     )
 
